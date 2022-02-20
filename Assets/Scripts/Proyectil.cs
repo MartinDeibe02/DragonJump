a426b270779ee428e2e14384778dc2b02f818972
Martin <martindeibe@gmail.com>
2022-02-20 15:36:43 +0100

    Creado nivel 1

--- Assets/Scripts/Proyectil.cs
+++ Assets/Scripts/Proyectil.cs
@@ -8,19 +8,19 @@ public class Proyectil : MonoBehaviour
     private float direccion;
     private bool hit;
     private float tiempoVida;
+
     private Animator anim;
     private BoxCollider2D boxCollider;
 
-
-    private void Start(){
+    private void Awake(){
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update(){
         if (hit) return;
-        float velocidad = speed * Time.deltaTime * direccion;
-        transform.Translate(velocidad, 0, 0);
+        float movementSpeed = speed * Time.deltaTime * direccion;
+        transform.Translate(movementSpeed, 0, 0);
 
         tiempoVida += Time.deltaTime;
         if (tiempoVida > 5) gameObject.SetActive(false);
@@ -32,21 +32,21 @@ public class Proyectil : MonoBehaviour
         anim.SetTrigger("explotar");
     }
 
-
     public void SetDireccion(float _direccion){
         tiempoVida = 0;
-
+        
         direccion = _direccion;
         gameObject.SetActive(true);
         hit = false;
         boxCollider.enabled = true;
+
         float escalaX = transform.localScale.x;
         if (Mathf.Sign(escalaX) != _direccion)
             escalaX = -escalaX;
 
         transform.localScale = new Vector3(escalaX, transform.localScale.y, transform.localScale.z);
     }
-    
+
     private void Desactivar(){
         gameObject.SetActive(false);
     }
